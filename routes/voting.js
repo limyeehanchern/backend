ed2f4659b962ef07961c2827c08d7921d3c45e62
limyeechern <66681646+limyeechern@users.noreply.github.com>
2022-06-29 14:43:40 +0800

    Update voting.js

--- routes/voting.js
+++ routes/voting.js
@@ -7,14 +7,14 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-  console.log(req.body);
-  // pool.query(
-  //   "CREATE TABLE question(id SERIAL PRIMARY KEY,content text NOT NULL,optionZero VARCHAR(250) NOT NULL,optionOne VARCHAR(250) NOT NULL,qid INTEGER, salt VARCHAR(10),result INTEGER,startDate date,endDate date)",
-  //   (err, res) => {
-  //     if (err) {
-  //       console.log("Tables already created");
-  //       return;
-  //     }
+  const {address, option, unix, qid} = req.body
+  pool.query(
+    "INSERT INTO votes (address, option, unix, salt, qid) VALUES (SELECT ${address}, ${option}, ${unix}, salt, ${qid} FROM question WHERE qid = ${qid})",
+    (err, res) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
 });
 
 module.exports = router;
