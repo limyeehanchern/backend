cfa25660f1ede3d87afec6d95c546b8a58bdcd95
limyeechern <66681646+limyeechern@users.noreply.github.com>
2022-06-29 14:33:47 +0800

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,29 +1,9 @@
-CREATE DATABASE limmy;
+CREATE TABLE question(qid SERIAL PRIMARY KEY,content text NOT NULL,optionZero VARCHAR(250) NOT NULL,optionOne VARCHAR(250) NOT NULL, salt VARCHAR(10),result INTEGER,startDate date,endDate date)
 
-CREATE TABLE question(
-  id SERIAL PRIMARY KEY,
-  content text NOT NULL,
-  optionZero VARCHAR(250) NOT NULL,
-  optionOne VARCHAR(250) NOT NULL,
-  qid INTEGER, salt VARCHAR(10),
-  result INTEGER,
-  startDate date,
-  endDate date
-)
-
-CREATE TABLE votes(
-  address VARCHAR(50), 
-  option INTEGER,
-  unix VARCHAR(50), 
-  salt VARCHAR(10), 
-  qid INTEGER, 
-  foreignKey INTEGER,
-  FOREIGN KEY (foreignKey) REFERENCES question ON DELETE CASCADE ON UPDATE CASCADE
-)
-
+CREATE TABLE votes(address VARCHAR(50) NOT NULL, option INTEGER NOT NULL,unix VARCHAR(50) NOT NULL, salt VARCHAR(10) NOT NULL, qid INTEGER NOT NULL, FOREIGN KEY (qid) REFERENCES question ON DELETE CASCADE ON UPDATE CASCADE)
 
 //inserting votes
-INSERT INTO votes (address, option, unix, salt) VALUES (SELECT ${address}, ${option}, ${unix}, salt FROM question WHERE qid = ${qid})
+INSERT INTO votes (address, option, unix, salt, qid) VALUES (SELECT ${address}, ${option}, ${unix}, salt, ${qid} FROM question WHERE qid = ${qid})
 
 //inserting questions
 INSERT INTO question(content, optionZero, optionOne, salt) VALUES (${content}, ${optionZero}, ${optionOne}, ${salt})
@@ -33,4 +13,11 @@ SELECT content, optionZero, optionOne FROM question WHERE qid = ${qid}
 
 //get historical question
 SELECT content, optionZero, optionOne, result FROM question WHERE qid != ${qid} ORDER BY qid DESC
+
+//getting data for reveal
+SELECT address, option, unix, salt FROM votes WHERE qid = ${qid}
+
+//getting salt
+SELECT salt FROM question WHERE qid = ${qid}
+
 
